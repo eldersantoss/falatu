075a819156877d475e9f12335ae1fa4a47500f93
Elder Santos <elder_rn@hotmail.com>
2023-05-26 15:22:45 -0300

    fix: create profile when creating new user

--- profiles/api/v1/views.py
+++ profiles/api/v1/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
 
+from ...models import Profile
 from .serializers import UserSerializer
 
 
@@ -9,3 +10,7 @@ class UserCreateView(CreateAPIView):
     permission_classes = (AllowAny,)
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def perform_create(self, serializer):
+        user = serializer.save()
+        Profile.objects.create(user=user)
--- profiles/tests/test_views.py
+++ profiles/tests/test_views.py
@@ -3,11 +3,13 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 
+from profiles.models import Profile
+
 
 class UserCreateViewTests(APITestCase):
     def test_create_user(self):
         """
-        Should create a new user through UserCreateView.
+        Should create a new user and your profile
         """
 
         url = reverse("user_create")
@@ -20,16 +22,20 @@ class UserCreateViewTests(APITestCase):
         response = self.client.post(url, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(Profile.objects.count(), 1)
 
         user = User.objects.get()
+        profile = Profile.objects.get()
+
         self.assertEqual(user.username, data["username"])
         self.assertEqual(user.email, data["email"])
         self.assertTrue(user.check_password(data["password"]))
         self.assertEqual(user.first_name, data["first_name"])
+        self.assertEqual(user.profile, profile)
 
     def test_create_user_missing_required_field(self):
         """
-        Should not create the user with missing required fields through UserCreateView.
+        Should not create the user because required fields are missing
         """
 
         url = reverse("user_create")
@@ -44,7 +50,7 @@ class UserCreateViewTests(APITestCase):
 
     def test_create_user_invalid_data(self):
         """
-        Should not create user with invalid data through UserCreateView.
+        Should not create user because data is invalid
         """
 
         url = reverse("user_create")
