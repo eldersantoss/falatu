e5df38886011d70c1600380f9aa7e4f5e248c277
Elder Santos <elder_rn@hotmail.com>
2023-05-27 17:03:54 -0300

    chore: remove useless imports

--- posts/api/v1/views.py
+++ posts/api/v1/views.py
@@ -1,7 +1,5 @@
 from rest_framework.generics import CreateAPIView, ListAPIView
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from posts.models import Post
 
