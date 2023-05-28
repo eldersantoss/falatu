af5b05a4a3c282220c96dd5f3cb4e9dba67c0376
Elder Santos <elder_rn@hotmail.com>
2023-05-27 22:19:50 -0300

    fix: add STATIC_ROOT conf

--- falatu/settings.py
+++ falatu/settings.py
@@ -130,6 +130,7 @@ TIME_ZONE = "America/Recife"
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = str(BASE_DIR / "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
