07ddf109ce15ca01168f84b9f891cc007b9e6f7e
Elder Santos <elder_rn@hotmail.com>
2023-05-29 16:45:41 -0300

    chore: configure upload of media and static files to S3

--- falatu/urls.py
+++ falatu/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -6,3 +8,6 @@ urlpatterns = [
     path("api/v1/profiles/", include("profiles.urls")),
     path("api/v1/posts/", include("posts.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
